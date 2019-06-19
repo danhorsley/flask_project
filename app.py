ceb6b1fdb7cb10c1792dcd94a6deb203399d5224
danhorsley <34445739+danhorsley@users.noreply.github.com>
2019-06-19 13:05:54 +0100

    lecture follow along

---
+++ app.py
@@ -0,0 +1,10 @@
+"""main application and routing logic for twitoff"""
+from flask import flask
+
+def create_app():
+  """Create and configure instance of the Flask application"""
+    @app.route('/')
+    def root():
+    return 'Welcome to TwitOff!!'
+    
+return app
